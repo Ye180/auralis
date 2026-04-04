6fc25da293eb569e0df2def7aab5212dbd8115d3
Ouattara Youssouf Emmanuel <ouattarayoussoufemmanuel@gmail.com>
2026-04-04 16:56:30 +0000

    first commit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 const Home = () => {
 	return (
 		<div className="font-playfair">
+			<span className="text-2xl italic">Auralis</span>
 			<span className="text-2xl italic">Playfair Display</span>
 			<div className="font-nunito-sans">General Sans</div>
 		</div>
